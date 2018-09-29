0256c1f19a89798cd7adc7b1ecb241128160d2b9
MO mursi <jemkarry@outlook.com>
2018-09-29 01:47:50 +0000

    automatically backed up by learn

---
+++ lib/create.sql
@@ -0,0 +1,6 @@
+create table projects (id INTEGER primary key , 
+title TEXT ,
+category TEXT,
+funding_goal INTEGER,
+start_date datetime default current_timestamp,
+end_date datetime default current_timestamp);
