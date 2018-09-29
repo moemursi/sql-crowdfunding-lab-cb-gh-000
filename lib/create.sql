8e105c0280c62c4d4c3717a87ef767a9c21bc688
MO mursi <jemkarry@outlook.com>
2018-09-29 01:52:39 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -4,3 +4,12 @@ category TEXT,
 funding_goal INTEGER,
 start_date datetime default current_timestamp,
 end_date datetime default current_timestamp);
+
+create table users(id INTEGER primary key,
+name TEXT,
+age INTEGER);
+
+create table pledges(id INTEGER primary key,
+amount INTEGER,
+user_id INTEGER,
+project_id INTEGER);
